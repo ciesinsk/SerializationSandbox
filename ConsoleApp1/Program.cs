e46ed8dd72e9924e19a0610c556bca3acf3fb905
Frank <ciesinsk@gmail.com>
2024-12-12 09:58:02 +0100

    Added solution

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,175 @@
+﻿using ConsoleApp1;
+using MessagePack;
+using ProtoBuf;
+using System;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var i = 1024 * 1024 * 1024;
+
+            Console.WriteLine($"{i}");
+            Console.WriteLine($"{Int32.MaxValue}");
+
+            var a = new ClassA(42);
+            a.SetValue(42);
+
+            var r1 = TestAllSerializers(a, "1");
+
+            var c = new ClassC();
+            c.classA = a;
+
+            var r2 = TestAllSerializers(c, "2");
+
+
+            var b = new ClassB();
+            b.SetValue(42 - 5);
+
+            var r3 = TestAllSerializers(b, "3");
+
+           
+            a.SetC(c); // circular reference
+            //var r4 = TestAllSerializers(a, "4");
+            var bfA =  TestBinaryFormatter(a, "4");
+            var dcA = TestDataContractSerializer(a, "4");
+            
+            //var mpA = TestMessagePack(a, "4");
+            //var pbA = TestProtoBuf(a, "4");
+        }
+
+        private static List<(string ser, T? result)> TestAllSerializers<T>(T t, string fileID)
+        {
+            var results = new List<(string ser, T? result)>();
+
+            var daDc = TestDataContractSerializer<T>(t, $"ser{fileID}.xml");
+            AddTestResult(t, daDc, "dc", results);
+
+            var daBf = TestBinaryFormatter(t, $"binary{fileID}.dat");
+            AddTestResult(t, daBf, "bf", results);
+
+
+            var daMp = TestMessagePack(t, $"messagePack{fileID}.dat");
+            AddTestResult(t, daMp,"mp", results);
+
+            var daPb = TestProtoBuf(t, $"protobuf{fileID}.dat");
+            AddTestResult(t, daPb, "pb", results);
+
+            return results;
+        }
+
+        private static void AddTestResult<T>(T a, T daDc, string testName, List<(string ser, T? result)> results)
+        {
+            if (daDc == null || daDc.Equals(a) != true)
+            {
+                results.Add(($"{testName}-failed", daDc));
+            }
+            else
+            {
+                results.Add(($"{testName}-correct", daDc));
+            }
+        }
+
+        private static T? TestDataContractSerializer<T>(T t, string fileName)
+        {
+            using (MemoryStream stream1 = new MemoryStream())
+            {
+                // ser
+                DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+                serializer.WriteObject(stream1, t);
+
+                stream1.Position = 0;
+                var fs = new FileStream(fileName, FileMode.Create);
+                stream1.WriteTo(fs);
+                fs.Close();
+
+                // deser
+                stream1.Position = 0;
+
+                fs = new FileStream(fileName, FileMode.Open);
+                var da = (T?)serializer.ReadObject(fs);
+                fs.Close();
+
+                return da;
+            }
+        }
+
+        private static T TestBinaryFormatter<T>(T t, string fileName)
+        {
+            if(t == null)
+            {
+                throw new ArgumentNullException($"Parameter {nameof(t)} must not be null");
+            }
+
+            using (MemoryStream stream1 = new MemoryStream())
+            {
+                // ser
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+                BinaryFormatter serializer = new BinaryFormatter();
+#pragma warning restore SYSLIB0011 // Type or member is obsolete
+                serializer.Serialize(stream1, t);
+
+                stream1.Position = 0;
+                var fs = new FileStream(fileName, FileMode.Create);
+                stream1.WriteTo(fs);
+                fs.Close();
+
+                // deser
+                fs = new FileStream(fileName, FileMode.Open);
+                var dt = (T)serializer.Deserialize(fs);
+                fs.Close();
+
+                return dt;
+            }
+        }
+
+        private static T TestMessagePack<T>(T t, string fileName)
+        {
+            MessagePackSerializer.DefaultOptions = MessagePack.Resolvers.ContractlessStandardResolver.Options;
+            using (MemoryStream stream1 = new MemoryStream())
+            {
+                // ser
+                MessagePackSerializer.Serialize<T>(stream1, t);
+
+                stream1.Position = 0;
+                var fs = new FileStream(fileName, FileMode.Create);
+                stream1.WriteTo(fs);
+                fs.Close();
+
+                // deser
+                fs = new FileStream(fileName, FileMode.Open);
+                var dt = MessagePackSerializer.Deserialize<T>(fs);
+                fs.Close();
+
+                return dt;
+            }
+        }
+
+        private static T TestProtoBuf<T>(T t, string fileName)
+        {
+            using (MemoryStream stream1 = new MemoryStream())
+            {
+                // ser
+                Serializer.Serialize<T>(stream1, t);
+                
+                stream1.Position = 0;
+                var fs = new FileStream(fileName, FileMode.Create);
+                stream1.WriteTo(fs);
+                fs.Close();
+
+                // deser
+                fs = new FileStream(fileName, FileMode.Open);
+                var dt = Serializer.Deserialize<T>(fs);
+                fs.Close();
+
+                return dt;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/TestClasses.cs
@@ -0,0 +1,215 @@
+﻿using MessagePack;
+using ProtoBuf;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+namespace ConsoleApp1
+{
+
+    /// <summary>
+    /// A class that has several private members, no parameterless c'tor and a
+    /// am implicit property that does not need any serialization. Morover it can have a reference to a
+    /// ClassB object. 
+    /// </summary>
+    [Serializable]
+    [DataContract(IsReference = true)]
+    [MessagePackObject(AllowPrivate = true)]
+    [ProtoContract(SkipConstructor = true)]
+    public partial class ClassA : IEquatable<ClassA>
+    {
+        [Key(0)]
+        [ProtoMember(1)]
+        [DataMember]
+        private int m_value;
+
+        [Key(1)]
+        [ProtoMember(2)]
+        [DataMember]
+        private ClassB? m_classB = null;
+
+        [Key(2)]
+        [ProtoMember(3)]
+        [DataMember]
+        private ClassC? m_classC = null;
+
+        // note: no parameterless c'tor needed
+
+        [IgnoreMember]
+        [ProtoIgnore]
+        [IgnoreDataMember]
+        public int ObjectIdentifier { get { return RuntimeHelpers.GetHashCode(this); } }
+
+        public ClassA(int v)
+        {
+            m_value = v;
+            m_classB = new ClassB();
+            m_classB.SetValue(v - 5);
+        }
+
+        public void SetValue(int v)
+        {
+            m_value = v;
+            m_classB = new ClassB();
+            m_classB.SetValue(v - 5);
+        }
+
+        public void SetC(ClassC c)
+        {
+            c.classA = this;
+            m_classC = c;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj == null ||  obj.GetType() != typeof(ClassA))
+                return false;
+
+            return Equals((ClassA)obj);
+        }
+
+        public bool Equals(ClassA? other)
+        {
+            if (other == null)
+                return false;
+
+            if (m_value != other.m_value) 
+                return false;
+
+            if (m_classB == null && other.m_classB != null)
+                return false;
+
+            if (m_classB != null && other.m_classB == null)
+                return false;
+
+            if (m_classB != null && other.m_classB != null && m_classB.Equals(other.m_classB) == false)
+                return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }  
+    }
+
+    /// <summary>
+    /// A class that has a dictionary as a member. 
+    /// </summary>
+    [Serializable]
+    [MessagePackObject(AllowPrivate = true)]
+    [ProtoContract]
+    public partial class ClassB: IEquatable<ClassB>
+    {
+        [Key(0)]
+        [ProtoMember(1)]
+        private int m_value;
+
+        [Key(1)]
+        [ProtoMember(2)]
+        private Dictionary<int, int> m_counters = new Dictionary<int, int>();
+
+        [IgnoreMember]
+        [ProtoIgnore]
+        [IgnoreDataMember]
+        public int ObjectIdentifier { get { return RuntimeHelpers.GetHashCode(this); } }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj.GetType() != typeof(ClassB))
+                return false;
+
+            return Equals((ClassB)obj);
+        }
+
+        public bool Equals(ClassB? other)
+        {
+            if (other == null)
+                return false;
+
+            if (m_value != other.m_value)
+                return false;
+
+            if(m_counters.SequenceEqual(other.m_counters) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void SetValue(int v)
+        {
+            m_value = v;
+
+            if(m_counters.ContainsKey(v) == false)
+            {
+                m_counters[v] = 0;  
+            }
+
+            m_counters[v]++;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+
+    /// <summary>
+    /// A class that contains a reference to CLass as a simple property. Together with class a there can be a circular reference.
+    /// </summary>
+    [Serializable]
+    [MessagePackObject]
+    [ProtoContract]
+    public class ClassC : IEquatable<ClassC>
+    {
+        [Key(0)]
+        [ProtoMember(1)]
+        public ClassA? classA { get; set; } = null;
+
+        [IgnoreMember]
+        [ProtoIgnore]
+        [IgnoreDataMember]
+        public int ObjectIdentifier { get { return RuntimeHelpers.GetHashCode(this); } }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj.GetType() != typeof(ClassC))
+                return false;
+
+            return Equals((ClassC)obj);
+        }
+
+        public bool Equals(ClassC? other)
+        {
+            if (other == null)
+                return false;
+
+            if (this.classA == null && other.classA == null)
+            {
+                return true;
+            }
+
+            if (this.classA == null && other.classA != null)
+            {
+                return false;
+            }
+
+            if (this.classA != null && other.classA == null)
+            {
+                return false;
+            }
+
+            if (classA != null)
+                return classA.Equals(other.classA);
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+
+}
